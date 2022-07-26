75c6451ea302c0db22305f261feb5259364b470f
WickedJack99 <aaronmoser@t-online.de>
2022-07-26 13:54:15 +0200

    0001

---
+++ exercises/exercise00.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file exercise00.cpp
+ * @author Wicked Jack ()
+ * @brief 
+ * @version 0.1
+ * @date 2022-07-26
+ * 
+ */
+
+//This is a one line comment
+/**
+ * This
+ * is
+ * a
+ * multiline
+ * comment.
+ */
+#include <iostream> //Include library which contains cout to print string to console.
+using namespace std; //Allows to shorten some writing, instead of writing std::cout we can write cout.
+int main() //main-function, returns int-value
+{
+    cout << "Hello World\n"; //Print string to console
+    return 0; //Return code
+}
